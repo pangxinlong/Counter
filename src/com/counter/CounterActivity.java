7fb1163f811f19b6c59b7aac378f6144ac488337
pangxinlong <670116586@qq.com>
2013-12-19 14:14:20 +0800

    five

--- src/com/counter/CounterActivity.java
+++ src/com/counter/CounterActivity.java
@@ -14,153 +14,96 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class CounterActivity extends Activity implements OnClickListener {
-	public TextView tv;
-	public Button button1,button2,button3,button4,button5,
-				  button6,button7,button8,button9,button0,
-				  buttonadd,buttonminus,buttontimes,buttondivided,buttonresult,buttonclean;
-	public static List<String> array= new ArrayList<String>();
-	String data1,data2,operator=null;
-	ArrayList myarraylist=new ArrayList<String>();
+	public TextView showView;
+	ArrayList myarraylist = new ArrayList<String>();
+	private static final int TYPE_OPERATOR = -1;// 符号
+	private static final int TYPE_EMPTY = 0;// 空
+	private static final int TYPE_NUMBER = 1;// 数字
+
+	// 初始化idList
+	int idList[] = { R.id.button0, R.id.button1, R.id.button2, R.id.button3,
+			R.id.button4, R.id.button5, R.id.button6, R.id.button7,
+			R.id.button8, R.id.button9, R.id.add, R.id.minus, R.id.times,
+			R.id.divided, R.id.point, R.id.result, R.id.clean, R.id.textview };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO 自动生成的方法存根
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.counteractivity);
-		//绑定个个按钮
-		button1=(Button) findViewById(R.id.button1);
-		button2=(Button) findViewById(R.id.button2);
-		button3=(Button) findViewById(R.id.button3);
-		button4=(Button) findViewById(R.id.button4);
-		button5=(Button) findViewById(R.id.button5);
-		button6=(Button) findViewById(R.id.button6);
-		button7=(Button) findViewById(R.id.button7);
-		button8=(Button) findViewById(R.id.button8);
-		button9=(Button) findViewById(R.id.button9);
-		button0=(Button) findViewById(R.id.button0);
-		buttonclean=(Button) findViewById(R.id.clean);
-		buttonadd=(Button) findViewById(R.id.add);
-		buttonminus=(Button) findViewById(R.id.minus);
-		buttontimes=(Button) findViewById(R.id.times);
-		buttondivided=(Button) findViewById(R.id.divided);
-		buttonresult=(Button) findViewById(R.id.result);
-		tv=(TextView) findViewById(R.id.textview);
-		
-		//监听各各按钮
-		button0.setOnClickListener(this);
-		button1.setOnClickListener(this);
-		button2.setOnClickListener(this);
-		button3.setOnClickListener(this);
-		button4.setOnClickListener(this);
-		button5.setOnClickListener(this);
-		button6.setOnClickListener(this);
-		button7.setOnClickListener(this);
-		button8.setOnClickListener(this);
-		button9.setOnClickListener(this);
-		buttonclean.setOnClickListener(this);
-		buttonadd.setOnClickListener(this);
-		buttonminus.setOnClickListener(this);
-		buttontimes.setOnClickListener(this);
-		buttondivided.setOnClickListener(this);
-		buttonresult.setOnClickListener(this);
-		tv.setOnClickListener(this);
-		
+
+		// 监听各各按钮
+		for (int i = 0; i < idList.length; i++) {
+			findViewById(idList[i]).setOnClickListener(this);
+		}
 	}
-		//按钮事件
-		@Override
-		public void onClick(View v) {
-			String data="";
-			switch(v.getId()){
-				case R.id.button0:{
-					myarraylist.add("0");
-					show();
-				}break;
-				
-				case R.id.button1:{
-					myarraylist.add("1");
-					show();
-					}
-				break;
-				
-				case R.id.button2:{
-					myarraylist.add("2");
-					show();
-				}break;
-				
-				case R.id.button3:{
-					myarraylist.add("3");
-					show();
-				}break;
-				
-				case R.id.button4:{
-					myarraylist.add("4");
-					show();
-				}break;
-				
-				case R.id.button5:{
-					myarraylist.add("5");
-					show();
-				}break;
-				
-				case R.id.button6:{
-					myarraylist.add("6");
-					show();
-				}break;
-				
-				case R.id.button7:{
-					myarraylist.add("7");
-					show();
-				}break;
-				
-				case R.id.button8:{
-					myarraylist.add("8");
-					show();
-				}break;
-				
-				case R.id.button9:{
-					myarraylist.add("9");
-					show();
-				}break;
-				
-				case R.id.add:{
-					myarraylist.add("+");
-					show();
-				}break;
-				
-				case R.id.minus:{
-					myarraylist.add("-");
-					show();
-				}break;
-				
-				case R.id.times:{
-					myarraylist.add("*");
-					show();
-				}break;
-				
-				case R.id.divided:{
-					myarraylist.add("/");
-					show();
-				}break;
-				
-				case R.id.result:{
-					myarraylist.add("=");
-					show();
-				}break;
 
+	// 按钮事件
+	@Override
+	public void onClick(View v) {
+		if ((((Button) v).getText()).equals("=")) {
+			// voluation(myarraylist);
+			show();
+		} else {
+			char buttonChar;// 存放监听到的按键字符串
+			buttonChar = ((Button) v).getText().charAt(0);
+			if (judgeCharOrNumber(((Button) v).getText().charAt(0))) {
+				switch (judgeCharList(myarraylist)) {
+				case TYPE_EMPTY:
+					;
+					break;
+				case TYPE_OPERATOR:
+					myarraylist.set(myarraylist.size() - 1,
+							((Button) v).getText());
+					break;
+				case TYPE_NUMBER:
+					myarraylist.add(((Button) v).getText());
+					break;
+				}
+			} else {
+				myarraylist.add((String) ((Button) v).getText());
 			}
-		
+			show();
 		}
-		
-		public void show(){
-			String resultStr="";
-			for(int i=0;i<myarraylist.size();i++){
-				resultStr+=(String) myarraylist.get(i);
-				}	
-			tv.setText(resultStr);
-			tv.getText().toString();
+	}
+
+	public void show() {// 用来显示输入的内容
+		String resultStr = " ";
+		for (int i = 0; i < myarraylist.size(); i++) {
+			Log.e("!!!!!", (String) myarraylist.get(0));
+			resultStr += (String) myarraylist.get(i);
 		}
+		((TextView) findViewById(idList[17])).setText(resultStr);
+	}
+
+	public boolean judgeCharOrNumber(char input) {// 用来判断输入的字符是不是符号
+		if ((42 <= input) && (input <= 47)) {// 是符号
+			return true;
+		} else {// 不是符号
+			return false;
+		}
+	}
+
+	public int judgeCharList(ArrayList<String> cacheList) {// 输入是符号，判断list的最后一位输入是什么
+		if (cacheList.size() == 0) {// list为空
+			return TYPE_EMPTY;
+		} else {
+			char lastItem = cacheList.get(cacheList.size() - 1).charAt(0);
+			if (judgeCharOrNumber(lastItem)) {
+				return TYPE_OPERATOR;// list最后一位是符号
+			} else
+				return TYPE_NUMBER;// list最后一位是数字
+		}
+	}
+
+	/*
+	 * public void voluation(ArrayList<String> cacheList) {
+	 * 
+	 * }
+	 * 
+	 * public double count(double num1, double num2, String opreate) { switch
+	 * (opreate.charAt(0)) { case '+': num1 += num2; break; case '-': num1 -=
+	 * num2; break; case '*': num1 *= num2; break; case '/': num1 /= num2;
+	 * break; } return num1; }
+	 */
 }
-
-		
-	
-
-
